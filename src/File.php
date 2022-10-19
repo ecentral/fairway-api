335997e649b9cdc1efaa1d21e8a9a7535412d4a8
Christian Rodriguez Benthake <git@cben.co>
2022-10-19 19:44:12 +0200

    Add licence headers to php files

--- src/File.php
+++ src/File.php
@@ -2,6 +2,13 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the "fairway_api" library by eCentral GmbH.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
 namespace Fairway\FairwayFilesystemApi;
 
 abstract class File
@@ -14,8 +21,7 @@ abstract class File
     public function __construct(
         DriverClient $client,
         string $identifier
-    )
-    {
+    ) {
         $this->client = $client;
         $this->identifier = $identifier;
     }
